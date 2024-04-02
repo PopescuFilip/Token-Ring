2343b4806206b1c049e210fca7d29a614f8c7ac8
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-04-02 13:47:52 +0300

    added TokenRingNetwork class

---
+++ TokenRingNetwork/TokenRingNetwork.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "CircularLinkedList.h"
+#include "Computer.h"
+#include "Token.h"
+
+class TokenRingNetwork
+{
+public:
+	TokenRingNetwork(int noOfComputers);
+
+private:
+	Token m_token;
+	CircularLinkedList<Computer> m_computers;
+};
+
